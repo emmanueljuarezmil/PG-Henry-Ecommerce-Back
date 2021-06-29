1a5fe41cfbba0b715d5dcb4f16258be565c07257
eljugador <eljugador@random.random.wtf>
2021-06-29 15:18:18 -0300

    modificado:     www.js 	borrado:        ../control/db/index.js 	modificado:     ../db.js 	../models/

--- bin/www.js
+++ bin/www.js
@@ -25,12 +25,12 @@ const server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 // Ya acomodado con sinq a la db
+// server.on('error', onError),
+// server.on('listening', onListening)
 
 conn.sync({force: true}).then(() =>
-  // server.listen(port),
-  server.on('error', onError),
-  server.on('listening', onListening)
-).catch(console.log('algo no '))
+  server.listen(port)
+).then(() => { console.log(`funciona en el ${port}`)}).catch(console.log('algo no '))
 
 
 /**
--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/asd`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -15,10 +15,10 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/control/db'))
+fs.readdirSync(path.join(__dirname, '/models'))
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/control/db', file)));
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
@@ -30,16 +30,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Producto, Usuario, Vendedor } = sequelize.models;
+const { Producto, Vendedor, Usuario } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
 //RELACIONES!!!
-Vendedor.belongsToMany(Producto, {through: 'vendedor_producto' })
-Producto.belongsToMany(Vendedor, {through: 'vendedor_producto' })
-Usuario.belongsToMany(Producto, {through: 'comprador_producto' })
-Producto.belongsToMany(Usuario, {through: 'comprador_producto' })
+
 
 
 module.exports = {
