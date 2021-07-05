f7ccc5b29388b20c7d1616348b029cc86d7514d6
eljugador <eljugador@random.random.wtf>
2021-07-05 18:48:47 -0300

    modificado:     control/default/category.js 	modificado:     routes/default/category.js  ya esta el post de category y falta verificar los productos por categoria

--- routes/default/category.js
+++ routes/default/category.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const {productsByCategory, newCategory, addOrDeleteCategory, updateCategory, deleteCategory} = require('../../control/default/category.js')  // importar funcion para categorias
+const {productsByCategory, newCategory, addOrDeleteCategory, updateCategory, deleteCategory, getAllCategories} = require('../../control/default/category.js')  // importar funcion para categorias
 
 
 
@@ -8,6 +8,7 @@ const router = Router();
 
 router.get('/category/:catName', productsByCategory); //tiene que devolver los productos de esa categoria
 router.post('/category', newCategory); 
+router.get('/categories', getAllCategories)
 router.put('/category/product/:idCategory', addOrDeleteCategory);  //para agregar o sacar una categoria al producto
 router.put('/category/:idCategory', updateCategory);
 router.delete('/category/:idCategory', deleteCategory);
