fe1a4a7d8211ee3a7a144e9f2d46200d07b651c2
eljugador <eljugador@random.random.wtf>
2021-07-05 15:15:13 -0300

    modificado:     app.js modificado:     package.json

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const cors = requiere('cors')
 
 const indexRouter = require('./routes/default/index');
 // const usersRouter = require('./routes/user/index');
@@ -16,8 +17,10 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
+
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
