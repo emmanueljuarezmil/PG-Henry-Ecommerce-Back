75444e73912d9f8e823dc4662039227c41371a09
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-07-14 18:27:24 -0300

    fix get /products

--- control/default/products.js
+++ control/default/products.js
@@ -7,11 +7,13 @@ const itemsPerPage = 40
 
 async function getProducts (req, res, next) {
     let { name, page, orderBy, orderType, category} = req.query
-    if(name === '' || name === 'undefined' || name === undefined) name = ''
-    if(page === '' || page === 'undefined' || page === undefined) page = 1
-    if(orderBy === '' || orderBy === 'undefined' || orderBy === undefined) orderBy = 'name'
-    if(orderType === '' || orderType === 'undefined' || orderType === undefined) orderType = 'asc'
-    if(category === '' || category === 'undefined' || category === undefined) category = ''
+    const validate = ['null', undefined, 'undefined', '']
+    if(validate.includes(name)) name = ''
+    if(validate.includes(page)) page = 1
+    if(validate.includes(orderBy)) orderBy = 'name'
+    if(validate.includes(orderType)) orderType = 'asc'
+    if(validate.includes(category)) category = ''
+    console.log(name,category, page, orderBy, orderType)
     try {
         const count = await Product.findAll({
             where: {
