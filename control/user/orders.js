de573a6ea3ecbdcd1859be80d65954e36d9e0ff7
Giancucinotta <giancucinotta@gmail.com>
2021-07-16 23:11:28 -0300

    Fixings

--- control/user/orders.js
+++ control/user/orders.js
@@ -1,42 +1,43 @@
 const { User, Product, Order, Order_Line } = require('../../db.js');
+const { Op } = require('sequelize')
 
 const exclude = ['createdAt', 'updatedAt']
 
 const getAllOrders = async (req, res, next) => {
     // if (!req.query.status) return res.status(400).send("Status is required ")
     const status = req.query.status
-     try {
-         const orderByStatus = await Order.findAll(
-             status ?
-             {
-                where: {
-                    status: req.query.status                
-                }          
-            } :
-            {}
-         )
-         if (!orderByStatus.length) {
-             return res.status(400).json({ message: 'There are not orders with that status.' })
-         }
-         return res.status(200).json(orderByStatus)
-     } catch (error) {
-         next(error);
-     }
- };
+    try {
+        const orderByStatus = await Order.findAll(
+            status ?
+                {
+                    where: {
+                        status: req.query.status
+                    }
+                } :
+                {}
+        )
+        if (!orderByStatus.length) {
+            return res.status(400).json({ message: 'There are not orders with that status.' })
+        }
+        return res.status(200).json(orderByStatus)
+    } catch (error) {
+        next(error);
+    }
+};
 
- const userOrders = async (req, res, next) => {
+const userOrders = async (req, res, next) => {
     if (!req.params.idUser) return res.status(400).send("Correct idUser is required ")
     try {
         const userOrders = await Order.findAll({
             where: {
                 UserId: req.params.idUser,
-                status: 'cart'              
-            }          
+                status: 'cart'
+            }
         })
         if (!userOrders.length) {
             return res.status(400).json({ message: 'The user has not any order' })
         }
-        
+
         return res.status(200).json(userOrders)
     } catch (error) {
         next(error);
@@ -44,41 +45,54 @@ const getAllOrders = async (req, res, next) => {
 };
 
 const getOrderById = async (req, res, next) => {
-    const {id} = req.params
+    const { id } = req.params
     if (!id) return res.status(400).send("Order ID is required")
     try {
-        const order = await Order.findOne({
+        const order = await Order_Line.findAll({
             where: {
-                id
-            },
-            include: {
-                model: Product,
-                through: {
-                    attributes: []
-                },
-                attributes: {
-                    exclude
-                }
+                orderID: id
             }
         })
-        return res.send(order)
-    }catch(err) {
+
+        const ids = order.map(element => {
+            return {
+                id: element.productID,
+            }
+        })
+
+        const products = await Product.findAll({
+            where: {
+                [Op.or]: ids
+            },
+            attributes: ['name', 'photo', 'id', 'price']
+        })
+
+        for (let i = 0; i < order.length; i++) {
+            for (let j = 0; j < products.length; j++) {
+                if (order[i].productID === products[j].id) {
+                    products[j].dataValues.quantity = order[i].quantity
+                }
+            }
+        }
+
+        return res.send(products)
+    } catch (err) {
         return res.status(400).send(err)
     }
 };
 
 const updateOrder = async (req, res, next) => {
-    const {id} = req.params
-    const {products} = req.body    
-    if(!id) return res.status(400).send('El id de la orden es requerido')
-    if(!products) return res.status(400).send('Los productos a actualizar son requeridos')
+    const { id } = req.params
+    const { products } = req.body
+    if (!id) return res.status(400).send('El id de la orden es requerido')
+    if (!products) return res.status(400).send('Los productos a actualizar son requeridos')
     try {
         const orderToDelete = await Order.findByPk(id)
-        if(!orderToDelete) return res.status(400).send('El id de la orden enviada es inválido')
+        if (!orderToDelete) return res.status(400).send('El id de la orden enviada es inválido')
         const UserId = orderToDelete.UserId
         const user = await User.findByPk(UserId);
-        if(!user) return res.status(400).send('El usuario es inválido')
-        const verifiedProductsPromises = products.map(async productToAdd => {            
+        if (!user) return res.status(400).send('El usuario es inválido')
+        const verifiedProductsPromises = products.map(async productToAdd => {
             try {
                 const product = await Product.findByPk(productToAdd.id);
                 if (!product) {
@@ -87,36 +101,36 @@ const updateOrder = async (req, res, next) => {
                 if (product.stock < productToAdd.quantity) {
                     return 'No hay stock suficiente de alguno de los productos'
                 }
-            } catch(err){
+            } catch (err) {
                 console.error(err)
-                return err    
+                return err
             }
         })
-        const error = await Promise.all(verifiedProductsPromises).then(result => result).catch(err => err)        
+        const error = await Promise.all(verifiedProductsPromises).then(result => result).catch(err => err)
         const concatError = [...new Set(error.filter(element => element))].join('. ')
-        if(concatError) return res.status(400).send(concatError)
+        if (concatError) return res.status(400).send(concatError)
         await orderToDelete.destroy()
-        const order = await Order.create()        
+        const order = await Order.create()
         await user.addOrder(order);
-        await products.forEach(async productToAdd => {            
+        await products.forEach(async productToAdd => {
             try {
                 const product = await Product.findByPk(productToAdd.id);
                 const quantity = Number(productToAdd.quantity);
                 const price = product.price
                 await product.addOrder(order, { through: { orderId: order.id, quantity, price } })
-            } catch(err){
-                console.error(err)    
+            } catch (err) {
+                console.error(err)
             }
         })
         return res.send('La orden fue actualizada con éxito')
-    }catch(err) {
+    } catch (err) {
         return res.status(400).send(err)
     }
 };
- 
- module.exports = {
-     getAllOrders,
-     userOrders,
-     getOrderById,
-     updateOrder    
- }
+
+module.exports = {
+    getAllOrders,
+    userOrders,
+    getOrderById,
+    updateOrder
+}
