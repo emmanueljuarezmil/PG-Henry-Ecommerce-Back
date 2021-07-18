735cc7956573848c2d863f21a885da7e614e91aa
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-07-18 15:11:52 -0300

    set middlewares

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cookieParser());
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use((req, res, next) => {
-//   console.log(req.cookies);
+//   if(req.cookies) console.log(req.cookies);
 //   next();
 // });
 app.use('/', ind)
