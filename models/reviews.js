ab03d45238142f38ad8eef8061397c74634a4574
eljugador <eljugador@random.random.wtf>
2021-07-20 18:20:14 -0300

    modificado:     models/reviews.js

--- models/reviews.js
+++ models/reviews.js
@@ -2,12 +2,12 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('Review', {
-	id:{
-	type: DataTypes.UUID,
-	allowNull: false
-	}
+	    id:{
+	      type: DataTypes.UUID,
+	      allowNull: false
+	    },
       comment: {
-        type: DataTypes.TEXT
+        type: DataTypes.TEXT(255)
       },
       rating: {
         type: DataTypes.ENUM(0,1,2,3,4,5,6,7,8,9,10)
