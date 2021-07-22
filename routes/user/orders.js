e905000003f6709a186215da8cd356dd0e29f5ba
Emmanuel Juarez <emmanueljuarezmil@gmail.com>
2021-07-22 13:32:51 -0300

    change syntaxis error

--- routes/user/orders.js
+++ routes/user/orders.js
@@ -6,6 +6,6 @@ const router = Router();
 router.get('/orders/users/:idUser', userOrders);
 router.get('/orders', getAllOrders);
 router.get('/orders/:id', getOrderById);
-rotuer.put('/orders/:id', updateOrder)
+router.put('/orders/:id', updateOrder)
 
 module.exports = router;
