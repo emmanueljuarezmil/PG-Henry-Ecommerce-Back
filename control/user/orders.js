ccd31d85648618c7e7829f0a1bd8e5d90058f47b
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-07-22 11:59:25 -0300

    modularize checkout restart put /orders

--- control/user/orders.js
+++ control/user/orders.js
@@ -73,8 +73,56 @@ const getOrderById = async (req, res, next) => {
     }
 };
 
+const updateOrder = async (req, res, next) => {
+    const { id } = req.params
+    const { products } = req.body
+    if (!id) return res.status(400).send('El id de la orden es requerido')
+    if (!products) return res.status(400).send('Los productos a actualizar son requeridos')
+    try {
+        const orderToDelete = await Order.findByPk(id)
+        if (!orderToDelete) return res.status(400).send('El id de la orden enviada es inválido')
+        const UserId = orderToDelete.UserId
+        const user = await User.findByPk(UserId);
+        if (!user) return res.status(400).send('El usuario es inválido')
+        const verifiedProductsPromises = products.map(async productToAdd => {
+            try {
+                const product = await Product.findByPk(productToAdd.id);
+                if (!product) {
+                    return 'El id de alguno de los productos enviados es inválido'
+                };
+                if (product.stock < productToAdd.quantity) {
+                    return 'No hay stock suficiente de alguno de los productos'
+                }
+            } catch (err) {
+                console.error(err)
+                return err
+            }
+        })
+        const error = await Promise.all(verifiedProductsPromises).then(result => result).catch(err => err)
+        const concatError = [...new Set(error.filter(element => element))].join('. ')
+        if (concatError) return res.status(400).send(concatError)
+        await orderToDelete.destroy()
+        const order = await Order.create()
+        await user.addOrder(order);
+        await products.forEach(async productToAdd => {
+            try {
+                const product = await Product.findByPk(productToAdd.id);
+                const quantity = Number(productToAdd.quantity);
+                const price = product.price
+                await product.addOrder(order, { through: { orderId: order.id, quantity, price } })
+            } catch (err) {
+                console.error(err)
+            }
+        })
+        return res.send('La orden fue actualizada con éxito')
+    } catch (err) {
+        return res.status(400).send(err)
+    }
+};
+
 module.exports = {
     getAllOrders,
     userOrders,
-    getOrderById 
+    getOrderById,
+    updateOrder
 }
