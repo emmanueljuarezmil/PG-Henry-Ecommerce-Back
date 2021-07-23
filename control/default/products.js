1deef34ef00cef29e5645db03b8392b5acf21ab8
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-07-22 22:28:38 -0300

    fix reviews

--- control/default/products.js
+++ control/default/products.js
@@ -1,6 +1,8 @@
 const { Product, Category, Review, User } = require('../../db.js');
 const { v4: uuidv4 } = require('uuid');
 const { Op } = require("sequelize");
+const sequelize = require('sequelize')
+// const sequelize = require("sequelize")
 const productosmeli = require('../../bin/data/productsDB.json');
 
 const itemsPerPage = 40
@@ -94,7 +96,8 @@ async function getProductsById(req, res, next) {
                         attributes: [],
                     },
                     include: {
-                        model: User
+                        model: User,
+                        attributes: ['name', 'userName']
                     }
                 }
             ]
