a788cff57df24c2bb462196536b43d8d0f36d56a
Fernando Perez Diez <perezdiezf@gmail.com>
2021-07-23 23:46:31 -0300

    nodemailer

--- control/user/users.js
+++ control/user/users.js
@@ -1,5 +1,6 @@
 const { User } = require('../../db.js');
 const { v4: uuidv4 } = require('uuid');
+const axios = require('axios')
 
 const exclude = ['createdAt', 'updatedAt']
 
@@ -96,6 +97,10 @@ async function loginUser(req, res, next) {
               email,
               userName: username,
               hashedPassword: hashedpassword
+            })
+            await axios.post(`http://localhost:3000/user/sendmail?type=welcome`,{
+            name: username,
+            email
             })
             return res.send(newUser)
           }
